7603395ba05daa5e89133716689626e5f93f2c6c
Goggen94 <160511669+Goggen94@users.noreply.github.com>
2024-10-13 23:38:28 +0200

    Update scrape_flight.py

--- scraper/scrape_flight.py
+++ scraper/scrape_flight.py
@@ -143,13 +143,13 @@ if response.status_code == 200:
         <script>
           // Firebase-konfigurasjon
           const firebaseConfig = {{
-        apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+       apiKey: "AIzaSyDebqZBQtfPbR6Nx84HnfN6qLX7LrLMWJY",
+      authDomain: "paxbot-2b8b2.firebaseapp.com",
+      projectId: "paxbot-2b8b2",
+      storageBucket: "paxbot-2b8b2.appspot.com",
+      messagingSenderId: "315115333501",
+      appId: "1:315115333501:web:f9503954d0b067fb2ece14",
+      measurementId: "G-S0Y63P27KG"
           }};
 
           // Initialiser Firebase
