eb343284aa584d9f15710e2a07a5a6db3553ed82
Goggen94 <160511669+Goggen94@users.noreply.github.com>
2024-10-14 22:23:59 +0200

    Update scrape_flight.py

--- scraper/scrape_flight.py
+++ scraper/scrape_flight.py
@@ -142,6 +142,13 @@ if response.status_code == 200:
     html_output = f"""
     <html>
     <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PQ7QTDM5');</script>
+<!-- End Google Tag Manager -->
         <title>KEF Airport Departures</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="refresh" content="600">  <!-- Refresh every 10 minutes -->
@@ -226,6 +233,10 @@ if response.status_code == 200:
         </script>
     </head>
     <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQ7QTDM5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <div style="display: flex; justify-content: space-between; align-items: center;">
             <h2>KEF Airport Departures</h2>
             <a href="https://arr.paxnotes.com" id="departures-btn">Arrivals</a>
