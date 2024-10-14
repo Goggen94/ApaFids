2590ac504c8cc8caaefa72c2a0c6abd33c8a19d0
Goggen94 <160511669+Goggen94@users.noreply.github.com>
2024-10-14 16:29:43 +0200

    Update scrape_flight.py

--- scraper/scrape_flight.py
+++ scraper/scrape_flight.py
@@ -1,36 +1,6 @@
 import requests
 import os
 from datetime import datetime, timedelta
-import firebase_admin
-from firebase_admin import credentials, messaging
-
-# Initialize Firebase Admin SDK
-def initialize_firebase():
-    cred = credentials.Certificate({
-        "type": "service_account",
-        "project_id": "paxbot-2b8b2",
-        "private_key_id": os.getenv('FIREBASE_SERVICE_KEY'),
-        "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-        "client_email": "firebase-adminsdk-zfgam@paxbot-2b8b2.iam.gserviceaccount.com",
-        "client_id": "111223656380392685091",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zfgam@paxbot-2b8b2.iam.gserviceaccount.com"
-    })
-    firebase_admin.initialize_app(cred)
-
-# Function to send FCM notification
-def send_fcm_notification(token, title, message_body):
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title=title,
-            body=message_body
-        ),
-        token=token
-    )
-    response = messaging.send(message)
-    print('Successfully sent message:', response)
 
 # Function to get the current time and the time 24 hours ahead
 def get_time_range():
@@ -68,6 +38,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
 
         # Flight code-specific times
         if flight_number.startswith("OG"):
+            # OG flights
             checkin_opens_time = (sched_dt - timedelta(hours=3)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(hours=1)).strftime("%H:%M")
             boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
@@ -75,6 +46,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith(("W4", "W6", "W9")):
+            # W4, W6, W9 flights
             checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
             boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
@@ -82,6 +54,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith(("EZY", "EJU")):
+            # EZY, EJU flights
             checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
             go_to_gate_time = (event_dt - timedelta(minutes=60)).strftime("%H:%M")
@@ -89,6 +62,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             final_call_time = (event_dt - timedelta(minutes=30)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith(("TO", "HV")):
+            # TO, HV flights
             checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
             boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
@@ -96,6 +70,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith("NO"):
+            # NO flights
             checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
             boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
@@ -103,6 +78,15 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith("LS"):
+            # LS flights
+            checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
+            checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
+            boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
+            final_call_time = (event_dt - timedelta(minutes=30)).strftime("%H:%M")
+            name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
+            gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
+        elif flight_number.startswith("I2"):
+            # I2 flights
             checkin_opens_time = (sched_dt - timedelta(hours=2, minutes=30)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(minutes=40)).strftime("%H:%M")
             boarding_time = (event_dt - timedelta(minutes=40)).strftime("%H:%M")
@@ -110,6 +94,7 @@ def calculate_event_times(sched_time, event_time_for_gate, flight_number):
             name_call_time = (event_dt - timedelta(minutes=25)).strftime("%H:%M")
             gate_closed_time = (event_dt - timedelta(minutes=15)).strftime("%H:%M")
         elif flight_number.startswith("DL"):
+            # DL flights
             checkin_opens_time = (sched_dt - timedelta(hours=3)).strftime("%H:%M")
             checkin_closes_time = (sched_dt - timedelta(hours=1)).strftime("%H:%M")
             go_to_gate_time = (event_dt - timedelta(minutes=60)).strftime("%H:%M")
@@ -133,25 +118,96 @@ def generate_flightradar_link(flight_number, aircraft_reg):
     except:
         return "#"  # Return a placeholder link if there's an error
 
-# Send a push notification 15 and 5 minutes before the flight lands
-def schedule_push_notifications(flight, eta, fcm_token):
-    now = datetime.utcnow()
-    eta_dt = datetime.strptime(eta, "%Y-%m-%dT%H:%M:%SZ")
-    time_to_15min_before = eta_dt - timedelta(minutes=15) - now
-    time_to_5min_before = eta_dt - timedelta(minutes=5) - now
-
-    if time_to_15min_before.total_seconds() > 0:
-        send_fcm_notification(fcm_token, f"Flight {flight}", "Flight will land in 15 minutes!")
-    if time_to_5min_before.total_seconds() > 0:
-        send_fcm_notification(fcm_token, f"Flight {flight}", "Flight will land in 5 minutes!")
-
 # Check if the request was successful
 if response.status_code == 200:
     data = response.json()  # Parse the JSON data
-    fcm_token = os.getenv('YOUR_FCM_DEVICE_TOKEN')  # Your device token from GitHub secrets
-    initialize_firebase()
-
     previous_date = None  # Track the date to insert the yellow line when the day changes
+
+    # Generate HTML file with only departing flights handled by APA
+    html_output = f"""
+    <html>
+    <head>
+        <title>KEF Airport Departures</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="600">  <!-- Refresh every 10 minutes -->
+        <style>
+            body {{ background-color: #2c2c2c; color: white; font-family: Arial, sans-serif; font-size: 16px; }}
+            h2 {{ text-align: center; color: #f4d03f; font-size: 24px; padding: 10px; border-radius: 8px; background-color: #444444; margin-bottom: 15px; }}
+            table {{ width: 100%; margin: 15px auto; border-collapse: collapse; background-color: #333333; }}
+            th, td {{ padding: 8px 12px; text-align: left; border-bottom: 1px solid #666666; font-weight: bold; }}
+            th {{ background-color: #f4d03f; color: #333; font-weight: bold; border-radius: 5px; font-size: 14px; }}
+            td {{ font-size: 14px; }}
+            tr:nth-child(even) {{ background-color: #2c2c2c; }}
+            tr:hover {{ background-color: #444444; }}
+            #next-day {{ background-color: #f4d03f; color: black; font-weight: bold; text-align: center; padding: 8px; }}
+            #popup {{ display: none; position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); background-color: #444; padding: 8px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); z-index: 999; color: white; font-size: 16px; width: 40%;  /* Reduced size */ }}
+            #popup h3 {{ color: #f4d03f; font-size: 16px; margin-bottom: 5px; }}
+            #popup p {{ margin: 2px 0; font-size: 16px; display: flex; justify-content: space-between;  /* Vertical alignment */ }}
+            .info-container {{ display: flex; justify-content: space-between; align-items: flex-start; width: 100%; gap: 5px; }}
+            .info-container div {{ width: 48%; }}
+            .info-container div h3, .info-container div p {{ margin: 0; padding: 0; }}
+            #close-popup {{ cursor: pointer; color: #f4d03f; margin-top: 8px; text-align: center; display: block; }}
+            a {{ color: #f4d03f;  /* Set link color to yellow */ text-decoration: none; }}
+            a:hover {{ text-decoration: underline; }}
+            #departures-btn {{
+                margin-left: 20px;
+                padding: 10px 20px;
+                background-color: #444444;
+                color: #f4d03f;
+                font-weight: bold;
+                border-radius: 8px;
+                text-decoration: none;
+                cursor: pointer;
+                border: 2px solid #f4d03f;
+            }}
+            #last-updated {{
+                text-align: right;
+                color: #f4d03f;
+                font-size: 14px;
+                padding-right: 20px;
+            }}
+            @media only screen and (max-width: 600px) {{
+                #popup {{ width: 75%;  /* Adjusted for mobile */ padding: 8px; }}
+                .info-container {{ flex-direction: row; }}
+                .info-container div {{ width: 48%; }}
+                #departures-btn {{ margin-top: 15px; }}
+            }}
+        </style>
+        <script>
+            function showPopup(flight, goToGate, boarding, finalCall, nameCall, gateClosed, checkinOpens, checkinCloses, flightradarLink) {{
+                document.getElementById("popup").style.display = "block";
+                document.getElementById("flight-info").innerHTML = '<a href="' + flightradarLink + '" target="_blank">Flight: ' + flight + '</a>';
+                document.getElementById("go-to-gate").innerHTML = "Go to Gate: " + goToGate;
+                document.getElementById("boarding").innerHTML = "Boarding: " + boarding;
+                document.getElementById("final-call").innerHTML = "Final Call: " + finalCall;
+                document.getElementById("name-call").innerHTML = "Name Call: " + nameCall;
+                document.getElementById("gate-closed").innerHTML = "Gate Closed: " + gateClosed;
+                document.getElementById("checkin-opens").innerHTML = "Check-in opens: " + checkinOpens;
+                document.getElementById("checkin-closes").innerHTML = "Check-in closes: " + checkinCloses;
+            }}
+
+            function closePopup() {{
+                document.getElementById("popup").style.display = "none";
+            }}
+        </script>
+    </head>
+    <body>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h2>KEF Airport Departures</h2>
+            <a href="https://arr.paxnotes.com" id="departures-btn">Arrivals</a>
+        </div>
+        <div id="last-updated">Last updated: {datetime.now().strftime('%H:%M')}</div>
+        <table>
+            <tr>
+                <th>Flight</th>
+                <th>Destination</th>
+                <th>STD</th>
+                <th>ETD</th>
+                <th>Status</th>
+                <th>Stand</th>
+                <th>Gate</th>
+            </tr>
+    """
 
     for flight in data:
         destination = flight.get("destination_iata", "")
@@ -159,13 +215,86 @@ if response.status_code == 200:
         flight_number = flight.get("flight_prefix", "") + flight.get("flight_num", "")
         status = flight.get("status", "N/A")
         etd_time = flight.get("expected_time", "")
-        eta_time = flight.get("eta", "")  # Get ETA for the flight
+        aircraft_reg = flight.get("aircraft_reg", "N/A")  # Get A/C Reg for OG flights
 
-        if destination != "KEF" and handling_agent == "APA":  # Filter flights handled by APA
-            formatted_eta_time, _ = format_time(eta_time)
-            if formatted_eta_time and eta_time:
-                schedule_push_notifications(flight_number, eta_time, fcm_token)
+        # Remove N/A from ETD column
+        formatted_etd_time, _ = format_time(etd_time) if etd_time != "" else ("", None)
 
-    print("Notifications scheduled for relevant flights.")
+        # Filter flights handled by APA and departing from KEF
+        if destination != "KEF" and handling_agent == "APA":
+            destination_name = flight.get("destination", "N/A")
+            
+            # Format scheduled time (STD)
+            sched_time = flight.get("sched_time", "N/A")
+            formatted_sched_time, sched_date = format_time(sched_time)
+
+            # Use STD for Check-in Information and ETD for Gate Information if available
+            gate_sched_time = etd_time if etd_time else sched_time
+
+            # Calculate times for check-in and gate events
+            go_to_gate, boarding, final_call, name_call, gate_closed, checkin_opens, checkin_closes = calculate_event_times(sched_time, gate_sched_time, flight_number)
+
+            # Generate Flightradar link for W4, W6, W9 flights using flight number -1, and OG flights using A/C Reg
+            flightradar_link = generate_flightradar_link(flight_number, aircraft_reg)
+
+            row_click = f"onclick=\"showPopup('{flight_number}', '{go_to_gate}', '{boarding}', '{final_call}', '{name_call}', '{gate_closed}', '{checkin_opens}', '{checkin_closes}', '{flightradar_link}')\""
+
+            stand = flight.get("stand", "N/A")
+            gate = flight.get("gate", "N/A")
+            
+            # Insert yellow line when the day changes
+            if previous_date and sched_date != previous_date:
+                html_output += f"""
+                <tr id="next-day">
+                    <td colspan="7">Next Day Flights</td>
+                </tr>
+                """
+            
+            html_output += f"""
+                <tr {row_click}>
+                    <td>{flight_number}</td>
+                    <td>{destination_name}</td>
+                    <td>{formatted_sched_time}</td>
+                    <td>{formatted_etd_time}</td>
+                    <td>{status}</td>
+                    <td>{stand}</td>
+                    <td>{gate}</td>
+                </tr>
+            """
+
+            previous_date = sched_date  # Update the previous_date for next iteration
+
+    html_output += """
+        </table>
+
+        <div id="popup">
+            <div class="info-container">
+                <div>
+                    <h3>Check-in Information</h3>
+                    <p id="checkin-opens">Check-in opens:</p>
+                    <p id="checkin-closes">Check-in closes:</p>
+                </div>
+                <div>
+                    <h3>Gate Information</h3>
+                    <p id="flight-info">Flight:</p>
+                    <p id="go-to-gate">Go to Gate:</p>
+                    <p id="boarding">Boarding:</p>
+                    <p id="final-call">Final Call:</p>
+                    <p id="name-call">Name Call:</p>
+                    <p id="gate-closed">Gate Closed:</p>
+                </div>
+            </div>
+            <p id="close-popup" onclick="closePopup()">Close</p>
+        </div>
+    </body>
+    </html>
+    """
+
+    # Save the HTML file to the output directory
+    os.makedirs("scraper/output", exist_ok=True)
+    with open("scraper/output/index.html", "w", encoding="utf-8") as file:
+        file.write(html_output)
+
+    print("HTML file has been generated with departing flights handled by APA.")
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
