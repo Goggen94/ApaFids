ca405f492e57fe6e47648c6c8d6c54ad3319096c
Goggen94 <160511669+Goggen94@users.noreply.github.com>
2024-10-15 21:53:41 +0200

    Update scrape_flight.py

--- scraper/scrape_flight.py
+++ scraper/scrape_flight.py
@@ -203,9 +203,10 @@ if response.status_code == 200:
             <h2>KEF Airport Departures</h2>
             <a href="https://arr.paxnotes.com" id="departures-btn">Arrivals</a>
         </div>
-        html_output += f"""
+html_output += f"""
     <div id="last-updated">Last updated: {datetime.now().strftime('%H:%M:%S')}</div>
 """
+
 
         <table>
             <tr>
